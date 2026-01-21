7fc98aca4757c311e9c7e8a0e6fb257bef33ba07
wahabrej <awahabreja@gmail.com>
2026-01-21 17:17:15 +0600

    added the code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.firebase.appdistribution.gradle)
+    implementation(libs.ui.graphics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
