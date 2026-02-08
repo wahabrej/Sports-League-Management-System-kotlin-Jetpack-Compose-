f7cf0a7057921763874d088b86d6e543dffe3cd9
wahabrej <awahabreja@gmail.com>
2026-02-08 15:18:37 +0600

    added the code

--- app/src/main/java/com/example/myapplication/data/local/PrefsManager.kt
+++ app/src/main/java/com/example/myapplication/data/local/PrefsManager.kt
@@ -1,26 +1,25 @@
 package com.example.myapplication.data.local
-
 import android.content.Context
 import com.example.myapplication.data.remote.api.User
 import com.google.gson.Gson
-
 class PrefsManager(context: Context) {
-    private val prefs = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-    private val gson = Gson()
+private  val pref = context.getSharedPreferences("app_pref", Context.MODE_PRIVATE)
 
-    fun saveToken(token: String) {
-        prefs.edit().putString("auth_token", token).apply()
+    private  val gson = Gson()
+
+    fun saveToken(token : String){
+        pref.edit().putString("auth_token",token).apply()
+
     }
+    fun getToken(): String? = pref.getString("auth_token",null)
 
-    fun getToken(): String? = prefs.getString("auth_token", null)
 
-    fun saveUser(user: User?) {
+    fun saveUser(user:User?){
         val userJson = gson.toJson(user)
-        prefs.edit().putString("user_data", userJson).apply()
+        pref.edit().putString("user_data",userJson).apply()
     }
-
     fun getUser(): User? {
-        val userJson = prefs.getString("user_data", null) ?: return null
+        val userJson = pref.getString("user_data", null) ?: return null
         return try {
             gson.fromJson(userJson, User::class.java)
         } catch (e: Exception) {
@@ -28,7 +27,10 @@ class PrefsManager(context: Context) {
         }
     }
 
-    fun clear() {
-        prefs.edit().clear().apply()
+    fun  clear(){
+        pref.edit().clear().apply()
     }
+
+
+
 }
